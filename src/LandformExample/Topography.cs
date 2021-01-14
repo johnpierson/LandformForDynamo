18481b65cd318215073dcea3f7f196daabf7a29e
john pierson <sixtysecondrevit@gmail.com>
2021-01-14 10:29:22 -0700

    Create Point

---
+++ src/LandformExample/Topography.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Geometry;
+
+namespace LandformExample
+{
+    public class Topography
+    {
+
+        public static List<Point> GetPoints(Revit.Elements.Topography topography)
+        {
+            var points = topography.Points;
+            return points;
+        }
+
+    }
+}
