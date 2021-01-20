28b44aaa35cce71f9c4f9bd1acdba3bfa82c69ae
john pierson <sixtysecondrevit@gmail.com>
2021-01-20 15:14:50 -0700

    General Revisions

--- src/LandformExample/Topography.cs
+++ src/LandformExample/Topography.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
+using Dynamo.Graph.Nodes;
 using Revit.GeometryConversion;
 using RevitServices.Transactions;
 using Point = Autodesk.DesignScript.Geometry.Point;
 
 namespace LandformExample
 {
+    /// <summary>
+    /// Wrapper for topography
+    /// </summary>
     public class Topography
     {
         //to hide the overall topography class
@@ -16,6 +22,15 @@ namespace LandformExample
         {
         }
 
+        /// <summary>
+        /// This returns the topo points
+        /// </summary>
+        /// <param name="topography">The topography to get points from.</param>
+        /// <returns name="points">The topography points.</returns>
+        /// <search>
+        /// Topography.Points
+        /// </search>
+        [NodeCategory("Query")]
         public static List<Point> GetPoints(Revit.Elements.Topography topography)
         {
             //this example cheats and uses the built in Revit libraries to get the points
@@ -23,9 +38,54 @@ namespace LandformExample
             return points;
         }
 
-        public static Revit.Elements.Topography DeletePoints(Revit.Elements.Topography topography, List<Point> pointsToRemove)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="topography"></param>
+        /// <returns></returns>
+        public static List<Point> InteriorPoints(Revit.Elements.Topography topography)
         {
-            //cast the Revit.Elements.Topograph to the Autodesk.Revit.DB.TopographySurface version
+            //cast the Revit.Elements.Topography to the Autodesk.Revit.DB.TopographySurface version
+            var internalTopography = topography.InternalElement as TopographySurface;
+
+            //get the interior points, convert to list and cast as Dynamo points
+            return internalTopography.GetInteriorPoints().ToList().ToPoints();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="topography"></param>
+        /// <returns></returns>
+        //NOTE: This example demonstrates how to iterate through a list and convert, in the off-chance that there is not a converter.
+        public static List<Point> BoundaryPoints(Revit.Elements.Topography topography)
+        {
+            //cast the Revit.Elements.Topography to the Autodesk.Revit.DB.TopographySurface version
+            var internalTopography = topography.InternalElement as TopographySurface;
+
+            //our list to append dynamo points to
+            List<Point> boundaryPoints = new List<Point>();
+
+            //get the Autodesk.Revit.DB.XYZ points
+            var revitPoints = internalTopography.GetBoundaryPoints();
+
+            //iterate through and convert to dynamo points and add to our output list
+            foreach (var pt in revitPoints)
+            {
+                boundaryPoints.Add(pt.ToPoint());
+            }
+
+            return boundaryPoints;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="topography"></param>
+        /// <param name="pointsToRemove"></param>
+        public static void DeletePoints(Revit.Elements.Topography topography, List<Point> pointsToRemove)
+        {
+            //cast the Revit.Elements.Topography to the Autodesk.Revit.DB.TopographySurface version
             var internalTopography = topography.InternalElement as TopographySurface;
             //get the document related to the topography
             //TIP: (this method is useful because it retrieves the related document rather than just the current one)
@@ -40,7 +100,7 @@ namespace LandformExample
             //start a topography edit scope
             TopographyEditScope editScope = new TopographyEditScope(doc, "Delete Points");
             editScope.Start(internalTopography.Id);
-            
+
             //create and start a transaction to make a change to the topography
             Transaction transaction = new Transaction(doc);
             transaction.Start("Start deleting points.");
@@ -54,8 +114,31 @@ namespace LandformExample
             //commit the edit
             editScope.Commit(new TopographyEditFailuresPreprocessorVerbose());
 
-            //return the original topography element after we modified it.
-            return topography;
+
+            //set the result in appdomain
+            List<object> newData = new List<object> { DateTime.Now, "Succesfully deleted points.", topography };
+            SetData(newData);
+        }
+
+        public static List<List<object>> TopographyEditResults(bool toggle = true)
+        {
+            return AppDomain.CurrentDomain.GetData("Landform Results") as List<List<object>>;
+        }
+
+        private static void SetData(List<object> data)
+        {
+            if (AppDomain.CurrentDomain.GetData("Landform Results") is null)
+            {
+                List<List<object>> newData = new List<List<object>> { data };
+                AppDomain.CurrentDomain.SetData("Landform Results", newData);
+            }
+            else
+            {
+                List<List<object>> currentData = AppDomain.CurrentDomain.GetData("Landform Results") as List<List<object>>;
+                currentData.Add(data);
+
+                AppDomain.CurrentDomain.SetData("Landform Results", currentData);
+            }
         }
     }
 
